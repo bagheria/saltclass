0da40a24af95b1c1fa1cbabfb32e09fc952fb776
Bagheri <a.bagheri@uu.nl>
2019-01-14 15:10:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
+from setuptools import setup
 
+setup(name='sltcls',
+      version='0.1',
+      description='Short/Long Text Classifier using clustering-based enrichment',
+      url='https://github.com/bagheria/sltcls',
+      author='Ayoub Bagheri',
+      author_email='ayoub.bagheri@gmail.com',
+      license='MIT',
+      packages=['sltcls'],
+      zip_safe=False)
