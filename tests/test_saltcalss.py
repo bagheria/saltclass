7f135c1ff92134db5305e0537451022f5ba80ed4
Bagheri <a.bagheri@uu.nl>
2019-01-30 22:44:55 +0100

    Update test_saltcalss.py

--- tests/test_saltcalss.py
+++ tests/test_saltcalss.py
@@ -1,9 +1,14 @@
 import pytest
-from saltclass import SALT
+from saltclass import saltclass
 
 
-def test_is_instance(self):
+def test_is_instance():
     train_X = [[10, 0, 0], [0, 20, 0], [4, 13, 5]]
     train_y = [0, 1, 1]
     vocab = ['statistics', 'medicine', 'crime']
-    assert(isinstance(SALT(train_X, train_y, vocabulary=vocab, language='en')))
+    assert isinstance(saltclass.SALT(train_X, train_y, vocabulary=vocab, language='en'), saltclass.SALT)
+
+
+def test_test_saltclass():
+    salt = saltclass.SALT([[10, 0, 0], [0, 20, 0]], [0, 1], vocabulary=['statistics', 'medicine'])
+    assert(salt.language == 'nl')
